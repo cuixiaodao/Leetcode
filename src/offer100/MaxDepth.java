2cebd7507ae57e3153626c3701ab51b6fbd4bcbc
CuiXiaoDao <cuixiaodao@outlook.com>
2021-08-17 15:08:20 +0800

    Update MaxDepth.java

--- src/offer100/MaxDepth.java
+++ src/offer100/MaxDepth.java
@@ -1,1 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
+        int levelNodeNum;
+        int depth = 0;
+        while(!queue.isEmpty()) {
+            levelNodeNum = queue.size();
+            for(int i = 0; i < levelNodeNum; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+
+    }
+}
